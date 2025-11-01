7be46da70a79fd4e60e6ada3a450120e12da67ae
Richard Prab <richieprab@gmail.com>
2025-11-01 17:14:01 +0800

    feat: refined FE and BE

--- auroramartproject/urls.py
+++ auroramartproject/urls.py
@@ -10,10 +10,11 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("home.urls")),
+    path("accounts/", include("accounts.urls")),
     path("products/", include("products.urls")),
     path("cart/", include("cart.urls")),
     path("orders/", include("orders.urls")),
-    path("accounts/", include("accounts.urls")),
+    path("notifications/", include("notifications.urls")),  # Add this
 ]
 
 # Serve static and media files during development
